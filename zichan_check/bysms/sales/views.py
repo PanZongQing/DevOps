cff77f6dbcb01a3d2283e4aacefaa457a6996e24
PanZongQing <176578202@qq.com>
2022-07-25 18:46:01 +0800

    增加飞行游戏项目

--- zichan_check/bysms/sales/views.py
+++ zichan_check/bysms/sales/views.py
@@ -1,7 +1,49 @@
 from django.http import HttpResponse
 from common.models import Customer
+from django.template import engines
 
+html_template = '''
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+table {
+    border-collapse: collapse;
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+</style>
+</head>
+    <body>
+        <table>
+        <tr>
+        <th>id</th>
+        <th>姓名</th>
+        <th>电话号码</th>
+        <th>地址</th>
+        </tr>
 
+        {% for customer in customers %}
+            <tr>
+
+            {% for name, value in customer.items %}            
+                <td>{{ value }}</td>            
+            {% endfor %}
+
+            </tr>
+        {% endfor %}
+
+        </table>
+    </body>
+</html>
+'''
+
+django_engine = engines['django']
+template = django_engine.from_string(html_template)
 
 def listorders(request):
     return HttpResponse('下面是系统中所有的订单信息...')
@@ -12,15 +54,23 @@ def listcustomers(request):
     # key 是字段名，value 是 字段值
     qs = Customer.objects.values()
 
+    ph = request.GET.get('phonenumber',None)
+
+    if ph:
+        qs = qs.filter(phonenumber=ph)
+
     # 定义返回字符串
-    retStr = ''
+    # retStr = ''
+    tableContent = ''
     for customer in qs:
+        tableContent += '<tr>'
         for name,value in customer.items():
-            retStr += f'{name} : {value} | '
+            # retStr += f'{name} : {value} | '
+            tableContent += f'<td>{value}</td>'
 
-        retStr += '<br>'
+        tableContent += '</tr>'
 
-    return HttpResponse(retStr)
+    return HttpResponse(html_template%tableContent)
 
 
 # Create your views here.
